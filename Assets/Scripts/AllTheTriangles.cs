5943dee9c76206153b9aad601f0e2e978fb3a422
og <ogugu@test.com>
2020-02-04 16:15:41 +0900

    [add] Triangle関連を追加
    頂点を指定しての三角形描画

---
+++ Assets/Scripts/AllTheTriangles.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter)), RequireComponent(typeof(MeshRenderer)) ]
+[ExecuteInEditMode]
+public class AllTheTriangles : MonoBehaviour
+{
+
+    [SerializeField] private Material material;
+    [SerializeField] private Vector3[] vs = new Vector3[3];
+
+    private List<Vector3> vertices;
+    private List<int> triangles;
+
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+    private Mesh mesh;
+
+    void Update()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        meshRenderer.material = material;
+
+        vertices = new List<Vector3>();
+        triangles = new List<int>();
+
+        CreateMesh();
+
+    }
+
+    private void CreateMesh()
+    {
+        mesh = new Mesh();
+
+        SetVertices();
+        SetTriangles();
+
+        mesh.SetVertices(vertices);
+        mesh.SetTriangles(triangles, 0);
+
+        meshFilter.sharedMesh = mesh;
+    }
+
+
+    private void SetVertices()
+    {
+        vertices.AddRange(vs);
+    }
+
+    private void SetTriangles()
+    {
+        triangles.Add(0);
+        triangles.Add(1);
+        triangles.Add(2);
+
+    }
+
+}
