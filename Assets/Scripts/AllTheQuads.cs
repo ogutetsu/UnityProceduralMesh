e65c31bae7e814d23785a23920a7f7e9e8dbdf82
og <ogugu@test.com>
2020-02-04 16:49:27 +0900

    [add] AllQuadsクラスを追加

---
+++ Assets/Scripts/AllTheQuads.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllTheQuads : AbstractMeshGenerator
+{
+
+    [SerializeField] private Vector3[] vs = new Vector3[4];
+
+    protected override void SetMeshNums()
+    {
+        numVertices = 4;
+        numTriangles = 6;
+    }
+
+
+    protected override void SetVertices()
+    {
+        vertices.AddRange(vs);
+    }
+
+    protected override void SetTriangles()
+    {
+        triangles.Add(0);
+        triangles.Add(3);
+        triangles.Add(2);
+
+        triangles.Add(0);
+        triangles.Add(1);
+        triangles.Add(3);
+    }
+
+    protected override void SetNormals()
+    {
+
+    }
+
+    protected override void SetTangents()
+    {
+
+    }
+
+    protected override void SetUVs()
+    {
+
+    }
+
+    protected override void SetVertexColors()
+    {
+
+    }
+
+
+}
