d3701ec22669922ed0751d74e3ab3c9c0f3e8ecb
og <ogugu@test.com>
2020-02-05 12:17:13 +0900

    [add]  NoiseGeneratorクラスを追加

---
+++ Assets/Scripts/NoiseGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseGenerator 
+{
+
+    public float GetValueNoise()
+    {
+        return Random.value;
+    }
+
+    public float GetPerlinNoise(float x, float z)
+    {
+        return Mathf.PerlinNoise(x, z);
+    }
+
+}
