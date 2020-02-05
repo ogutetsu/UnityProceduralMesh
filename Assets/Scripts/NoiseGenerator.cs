c196739fec4689fcb4caf5589de12a080f808c3a
og <ogugu@test.com>
2020-02-05 12:25:13 +0900

    [update] NoiseGeneratorにFractalNoiseを実装

--- Assets/Scripts/NoiseGenerator.cs
+++ Assets/Scripts/NoiseGenerator.cs
@@ -2,8 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoiseGenerator 
+public class NoiseGenerator
 {
+    private int octaves;
+    private float lacunarity;
+    private float gain;
+    private float perlinScale;
+
+    public NoiseGenerator() { }
+
+    public NoiseGenerator(int octaves, float lacunarity, float gain, float perlinScale)
+    {
+        this.octaves = octaves;
+        this.lacunarity = lacunarity;
+        this.gain = gain;
+        this.perlinScale = perlinScale;
+    }
 
     public float GetValueNoise()
     {
@@ -12,7 +26,26 @@ public class NoiseGenerator
 
     public float GetPerlinNoise(float x, float z)
     {
-        return Mathf.PerlinNoise(x, z);
+        return (2 * Mathf.PerlinNoise(x, z) - 1);
+    }
+
+    public float GetGractalNoise(float x, float z)
+    {
+        float fractalNoise = 0;
+        float frequency = 1;
+        float amplitude = 1;
+        for (int i = 0; i < octaves; i++)
+        {
+            float xVal = x * frequency * perlinScale;
+            float zVal = z * frequency * perlinScale;
+
+            fractalNoise += amplitude + GetPerlinNoise(xVal, zVal);
+
+            frequency *= lacunarity;
+            amplitude *= gain;
+        }
+
+        return fractalNoise;
     }
 
 }
