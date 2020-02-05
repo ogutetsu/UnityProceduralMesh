e19e3351a6e9cebcb6b5edcf3330c50f3a835267
og <ogugu@test.com>
2020-02-05 11:37:12 +0900

    [add] Tangent確認用クラスを追加

---
+++ Assets/Scripts/MeshTangentDisplayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+
+public class MeshTangentDisplayer : MonoBehaviour
+{
+    [SerializeField] private bool drawTangents;
+    [SerializeField] private float tangentLength = 0.5f;
+
+    void OnDrawGizmosSelected()
+    {
+        if (drawTangents)
+        {
+            Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+            if (mesh != null)
+            {
+                for (int i = 0; i < mesh.vertexCount; i++)
+                {
+                    Vector3 vertex = transform.TransformPoint(mesh.vertices[i]);
+                    Vector3 tangent = transform.TransformDirection(mesh.tangents[i]);
+
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawLine(vertex, vertex + tangentLength * tangent);
+                }
+            }
+
+
+        }
+    }
+}
